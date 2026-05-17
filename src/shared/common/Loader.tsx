98fb131212d2402e2d62efc67e467af96e0922bc
Aman Jainwal <jainwalaman@gmail.com>
2026-05-17 13:29:03 +0530

    fix style loader component

--- src/shared/common/Loader.tsx
+++ src/shared/common/Loader.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen w-full ">
+      <p className="text-lg font-medium ">Loading...</p>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
